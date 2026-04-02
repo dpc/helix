fc6ae13d1a652f8792f1a1b099974af33385756d
Rolo <RoloEdits@gmail.com>
2026-04-02 15:52:58 -0700

    feat: add breadcrumb handling

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -385,6 +385,8 @@ pub struct Config {
     pub whitespace: WhitespaceConfig,
     /// Persistently display open buffers along the top
     pub bufferline: BufferLine,
+    /// Persistently display breadcrumb along the top, below any bufferline.
+    pub breadcrumb: bool,
     /// Vertical indent width guides.
     pub indent_guides: IndentGuidesConfig,
     /// Whether to color modes with different colors. Defaults to `false`.
@@ -1130,6 +1132,7 @@ impl Default for Config {
             rulers: Vec::new(),
             whitespace: WhitespaceConfig::default(),
             bufferline: BufferLine::default(),
+            breadcrumb: false,
             indent_guides: IndentGuidesConfig::default(),
             color_modes: false,
             soft_wrap: SoftWrap {
