d1d2506da8fb06f5943ae2aff1ddfc7398fafb4a
Dawid Ciężarkiewicz <dpc@dpc.pw>
2026-04-18 18:32:10 -0700

    fix(lsp): don't exit non-zero on language server shutdown timeout

--- helix-term/src/application.rs
+++ helix-term/src/application.rs
@@ -1341,9 +1341,6 @@ impl Application {
 
         if self.editor.close_language_servers(None).await.is_err() {
             log::error!("Timed out waiting for language servers to shutdown");
-            errs.push(anyhow::format_err!(
-                "Timed out waiting for language servers to shutdown"
-            ));
         }
 
         errs
